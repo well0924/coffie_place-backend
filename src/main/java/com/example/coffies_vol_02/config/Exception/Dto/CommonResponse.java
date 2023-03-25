208781973d6ced9945f2de5e17204f9820620ba9
well0924 <well414965@gmail.com>
2023-03-25 14:48:16 +0900

    refactor rest api 공통 포맷작성

--- src/main/java/com/example/coffies_vol_02/config/Exception/Dto/CommonResponse.java
+++ src/main/java/com/example/coffies_vol_02/config/Exception/Dto/CommonResponse.java
@@ -1,2 +1,18 @@
-package com.example.coffies_vol_02.config.Exception.Dto;public class CommonResponse {
+package com.example.coffies_vol_02.config.Exception.Dto;
+
+import lombok.*;
+
+import javax.persistence.GeneratedValue;
+import java.io.Serializable;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommonResponse<T> implements Serializable {
+
+    private Integer status;
+
+    private T data;
 }
--- src/main/java/com/example/coffies_vol_02/member/controller/api/MemberApiController.java
+++ src/main/java/com/example/coffies_vol_02/member/controller/api/MemberApiController.java
@@ -1,5 +1,6 @@
 package com.example.coffies_vol_02.member.controller.api;
 
+import com.example.coffies_vol_02.config.Exception.Dto.CommonResponse;
 import com.example.coffies_vol_02.member.domain.dto.MemberDto;
 import com.example.coffies_vol_02.member.service.MemberService;
 import lombok.AllArgsConstructor;
@@ -8,8 +9,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @AllArgsConstructor
@@ -18,58 +20,56 @@ public class MemberApiController {
     private final MemberService memberService;
 
     @GetMapping("/list")
-    public ResponseEntity<?>memberList(@PageableDefault(sort = "id",direction = Sort.Direction.DESC,size = 5) Pageable pageable){
+    public CommonResponse<?> memberList(@PageableDefault(sort = "id",direction = Sort.Direction.DESC,size = 5) Pageable pageable){
         Page<MemberDto.MemberResponseDto> list = memberService.findAll(pageable);
-        return new ResponseEntity<>(list,HttpStatus.OK);
+        return new CommonResponse<>(HttpStatus.OK.value(),list);
     }
 
     @GetMapping("/detail/{id}")
-    public ResponseEntity<?>memberDetail(@PathVariable("id")Integer userIdx){
-        MemberDto.MemberResponseDto detatil = memberService.findMemberById(userIdx);
-        return new ResponseEntity<>(detatil,HttpStatus.OK);
+    public CommonResponse<?>memberDetail(@PathVariable("id")Integer userIdx){
+        MemberDto.MemberResponseDto detail = memberService.findMemberById(userIdx);
+        return new CommonResponse<>(HttpStatus.OK.value(),detail);
     }
 
     @PostMapping("/memberjoin")
-    public ResponseEntity<?>memberJoin(@RequestBody MemberDto.MemberCreateDto dto){
+    public CommonResponse<?>memberJoin(@Valid @RequestBody MemberDto.MemberCreateDto dto){
         int JoinResult = memberService.memberSave(dto);
-        return new ResponseEntity<>(JoinResult,HttpStatus.OK);
+        return new CommonResponse<>(HttpStatus.OK.value(),JoinResult);
     }
 
-    @PutMapping("/memberUpdate/{id}")
-    public ResponseEntity<?>memberUpdate(@PathVariable("id") Integer userIdx, @RequestBody MemberDto.MemberCreateDto dto){
+    @PatchMapping("/memberUpdate/{id}")
+    public CommonResponse<?>memberUpdate(@PathVariable("id") Integer userIdx,@RequestBody MemberDto.MemberCreateDto dto){
         int UpdateResult = memberService.memberUpdate(userIdx,dto);
-        return new ResponseEntity<>(UpdateResult,HttpStatus.OK);
+        return new CommonResponse<>(HttpStatus.OK.value(),UpdateResult);
     }
 
     @DeleteMapping("/memberDelete/{id}")
-    public ResponseEntity<?>memberDelete(@PathVariable("id") Integer userIdx){
+    public CommonResponse<?>memberDelete(@PathVariable("id") Integer userIdx){
         memberService.memberDelete(userIdx);
-        return new ResponseEntity<>("Delete O.K",HttpStatus.OK);
+        return new CommonResponse<>(HttpStatus.OK.value(),"Delete O.K");
     }
 
     @GetMapping("/findid/{name}/{email}")
-    public ResponseEntity<?>findUserID(@PathVariable(value = "name") String userName,@PathVariable("email") String userEmail){
+    public CommonResponse<?>findUserID(@PathVariable(value = "name") String userName,@PathVariable("email") String userEmail){
         String findUser =memberService.findByMembernameAndUseremail(userName,userEmail);
-        return new ResponseEntity<>(findUser,HttpStatus.OK);
+        return new CommonResponse<>(HttpStatus.OK.value(),findUser);
     }
 
     @GetMapping("/idduplicated/{id}")
-    public ResponseEntity<?>userIdDuplicated(@PathVariable("id")String userId){
+    public CommonResponse<?>userIdDuplicated(@PathVariable("id")String userId){
         Boolean result = memberService.existsByUserId(userId);
-        return new ResponseEntity<>(result,HttpStatus.OK);
+        return new CommonResponse<>(HttpStatus.OK.value(),result);
     }
 
     @GetMapping("/emailduplicated/{email}")
-    public ResponseEntity<?>userEmailDuplicated(@PathVariable("email")String userEmail){
+    public CommonResponse<?>userEmailDuplicated(@PathVariable("email")String userEmail){
         Boolean result = memberService.existByUserEmail(userEmail);
-        return new ResponseEntity<>(result,HttpStatus.OK);
+        return new CommonResponse<>(HttpStatus.OK.value(),result);
     }
 
-    @PutMapping("/newpassword/{id}")
-    public ResponseEntity<?>passwordChange(@PathVariable("id")Integer id,@RequestBody MemberDto.MemberCreateDto dto){
+    @PatchMapping("/newpassword/{id}")
+    public CommonResponse<?>passwordChange(@PathVariable("id")Integer id,@RequestBody MemberDto.MemberCreateDto dto){
         int updateResult = memberService.updatePassword(id,dto);
-        return new ResponseEntity<>(updateResult,HttpStatus.OK);
+        return new CommonResponse<>(HttpStatus.OK.value(),updateResult);
     }
-
-
 }
