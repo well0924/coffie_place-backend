e9e1412385fa08541b6d31d25dcd7e178175628b
well0924 <well414965@gmail.com>
2023-03-25 14:58:20 +0900

    refactor js파일 분리

---
+++ src/main/resources/static/js/member/searchId.js
@@ -0,0 +1,71 @@
+/*
+*  회원 아이디 찾기 페이지
+*/
+
+//name유효성 검사o.k
+function validation(){
+    let name = document.getElementById('user_name').value;
+
+    if(name.trim().length==0){
+        alert('이름을 입력해주세요.');
+        return false;
+    }
+    return true;
+}
+//id유효성검사 o.k
+function validid(){
+    let id = document.getElementById('user_id').value;
+
+    if(id.trim().length==0){
+        alert('아이디를 입력해주세요.');
+        return false;
+    }
+    return true;
+}
+//이메일 형식 체크o.k
+function validemail(){
+    //이메일 입력값
+    let useremail = document.getElementById('user_email').value;
+    //이메일 양식 확인하는 정규식
+    let regEmail = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+
+    //형식검사
+    if(useremail.trim().length == 0){
+        alert('이메일을 입력해주세요.');
+        return false;
+    }
+
+    if(!regEmail.test(useremail)){
+        alert('이메일 형식이 아닙니다.');
+        return false;
+    }
+    return true;
+}
+
+//아이디 찾기 o.k
+function findId(){
+    let name = $('#user_name').val();
+    let email  = $('#user_email').val();
+
+    $.ajax({
+        url:'/api/member/findid/'+name+'/'+email,
+        type:'get',
+        dataType:'json',
+        contentType:'application/json; charset=UTF-8'
+    }).always(function(resp){
+        console.log(resp.responseText);
+
+        if(resp.responseText != null){
+            document.getElementById('msg').innerHTML = '</br> 찾으시는 아이디는'+resp.responseText+'입니다.';
+            document.getElementById('msg').style.color='blue';
+        }else{
+            document.getElementById('msg').innerHTML = '</br>찾으시는 아이디가 없거나 이름을 잘못 입력하셨습니다.';
+            document.getElementById('msg').style.color='red';
+        }
+    });
+
+}
+
+function changepwd(){
+    location.href='/page/login/pwdchange';
+}
