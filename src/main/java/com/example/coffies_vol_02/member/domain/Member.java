bbfbc11463fe0fbba539cf0fd5814c0a7c2ac838
well0924 <well414965@gmail.com>
2023-03-26 20:22:44 +0900

    refactor: Member 엔티티 컬럼 수정

--- src/main/java/com/example/coffies_vol_02/member/domain/Member.java
+++ src/main/java/com/example/coffies_vol_02/member/domain/Member.java
@@ -21,14 +21,14 @@ public class Member extends BaseTime {
     private String memberName;
     private String userPhone;
     private String userGender;
-    private Integer userAge;
+    private String userAge;
     private String userEmail;
     private String userAddr1;
     private String userAddr2;
     @Enumerated(EnumType.STRING)
     private Role role;
     @Builder
-    public Member(Integer id,String userId,String password,String memberName,String userPhone,String userGender,Integer userAge,String userEmail,String userAddr1,String userAddr2,Role role){
+    public Member(Integer id,String userId,String password,String memberName,String userPhone,String userGender,String userAge,String userEmail,String userAddr1,String userAddr2,Role role){
         this.id = id;
         this.userId = userId;
         this.password = password;
@@ -44,7 +44,7 @@ public class Member extends BaseTime {
         this.getUpdatedTime();
     }
 
-    //회원 수정
+    //회원 수정(Dirty Checking)
     public void updateMember(MemberDto.MemberCreateDto memberCreateDto){
         this.userId = memberCreateDto.getUserId();
         this.memberName = memberCreateDto.getMemberName();
