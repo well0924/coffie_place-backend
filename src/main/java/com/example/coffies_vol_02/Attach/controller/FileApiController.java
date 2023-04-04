28304151dbcef93ba73378cff25705e221d2eafd
well0924 <well414965@gmail.com>
2023-04-04 16:21:44 +0900

    feat: 공지게식르 파일 목록 기능 추가

--- src/main/java/com/example/coffies_vol_02/Attach/controller/FileApiController.java
+++ src/main/java/com/example/coffies_vol_02/Attach/controller/FileApiController.java
@@ -1,6 +1,7 @@
 package com.example.coffies_vol_02.Attach.controller;
 
 import com.example.coffies_vol_02.Attach.domain.AttachDto;
+import com.example.coffies_vol_02.Attach.repository.AttachRepository;
 import com.example.coffies_vol_02.Attach.service.AttachService;
 import com.example.coffies_vol_02.Config.Exception.Dto.CommonResponse;
 import lombok.AllArgsConstructor;
@@ -13,13 +14,17 @@ import java.util.List;
 @AllArgsConstructor
 @RequestMapping("/api/file")
 public class FileApiController {
-
     private final AttachService attachService;
 
     @GetMapping("/filelist")
     public CommonResponse<?>fileList(@PathVariable("board_id")Integer boardId) throws Exception {
-        List<AttachDto> list = attachService.filelist(boardId);
+        List<AttachDto> list = attachService.boardfilelist(boardId);
         return new CommonResponse<>(HttpStatus.OK.value(),list);
     }
 
+    @GetMapping("/noticefilelist")
+    public CommonResponse<?>noticefileList(@PathVariable("notice_id")Integer noticeId)throws Exception{
+        List<AttachDto>list = attachService.noticefilelist(noticeId);
+        return new CommonResponse<>(HttpStatus.OK.value(),list);
+    }
 }
--- src/main/java/com/example/coffies_vol_02/Attach/service/AttachService.java
+++ src/main/java/com/example/coffies_vol_02/Attach/service/AttachService.java
@@ -3,10 +3,6 @@ package com.example.coffies_vol_02.Attach.service;
 import com.example.coffies_vol_02.Attach.domain.Attach;
 import com.example.coffies_vol_02.Attach.domain.AttachDto;
 import com.example.coffies_vol_02.Attach.repository.AttachRepository;
-import com.example.coffies_vol_02.Board.domain.Board;
-import com.example.coffies_vol_02.Board.repository.BoardRepository;
-import com.example.coffies_vol_02.Config.Exception.ERRORCODE;
-import com.example.coffies_vol_02.Config.Exception.Handler.CustomExceptionHandler;
 import lombok.AllArgsConstructor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
@@ -14,23 +10,32 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class AttachService {
     private final AttachRepository attachRepository;
-    private final BoardRepository boardRepository;
 
     /*
-    *  파일 전체 목록
+    *  파일 전체 목록(자유게시판)
     */
     @Transactional(readOnly = true)
-    public List<AttachDto> filelist(@Param("id") Integer boardId)throws Exception{
-        Optional<Board> findBoard = Optional.ofNullable(boardRepository.findById(boardId).orElseThrow(() -> new CustomExceptionHandler(ERRORCODE.BOARD_NOT_FOUND)));
-
+    public List<AttachDto> boardfilelist(@Param("id") Integer boardId)throws Exception{
         List<Attach>list = attachRepository.findAttachBoard(boardId);
+        return getAttach(list);
+    }
 
+    /*
+    *  파일 전체 목록(공지 게시판)
+     */
+    @Transactional
+    public List<AttachDto>noticefilelist(@Param("id")Integer noticeId)throws Exception{
+        List<Attach>noticeList = attachRepository.findAttachNoticeBoard(noticeId);
+
+        return getAttach(noticeList);
+    }
+
+    private List<AttachDto> getAttach(List<Attach> list) {
         List<AttachDto>filelist = new ArrayList<>();
 
         for(Attach file : list){
