277798516c572a2d7ded7397a500fffbcbebfa26
well0924 <well414965@gmail.com>
2023-04-10 01:58:06 +0900

    feat : 메인 페이지 화면 작성 및 게시글 연결

--- src/main/java/com/example/coffies_vol_02/Main/MainController.java
+++ src/main/java/com/example/coffies_vol_02/Main/MainController.java
@@ -4,15 +4,16 @@ import com.example.coffies_vol_02.Board.domain.dto.BoardDto;
 import com.example.coffies_vol_02.Board.service.BoardService;
 import com.example.coffies_vol_02.Notice.domain.dto.NoticeBoardDto;
 import com.example.coffies_vol_02.Notice.service.NoticeService;
+import com.example.coffies_vol_02.Place.repository.PlaceRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
 
 @Controller
 @AllArgsConstructor
@@ -21,17 +22,17 @@ public class MainController {
     private final BoardService boardService;
     private final NoticeService noticeService;
 
-    @GetMapping("/mainpage")
-    public ModelAndView mainPage(Pageable pageable){
+    @GetMapping("/main")
+    public ModelAndView mainPage(@PageableDefault(size = 5,sort = "id",direction = Sort.Direction.DESC) Pageable pageable){
         ModelAndView  mv = new ModelAndView();
 
-        Page<BoardDto.BoardResponseDto> boardList= boardService.boardAll(pageable);
-        Page<NoticeBoardDto.BoardResponseDto>noticeList = noticeService.noticeList(pageable);
+        Page<BoardDto.BoardResponseDto> boardList = boardService.boardAll(pageable);
+        Page<NoticeBoardDto.BoardResponseDto>noticelist = noticeService.noticeList(pageable);
 
-        mv.addObject("boardList",boardList);
-        mv.addObject("noticelist",noticeList);
+        mv.addObject("boardlist",boardList);
+        mv.addObject("noticelist",noticelist);
 
-        mv.setViewName("/main/mian");
+        mv.setViewName("/mainPage/mainpage");
 
         return mv;
     }
