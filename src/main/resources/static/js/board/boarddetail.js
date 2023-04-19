9d3d59bc0e2df84b00ed9dcf790482f283d2155e
well0924 <well414965@gmail.com>
2023-04-19 15:00:34 +0900

    refactor : 자유게시판 좋아요 기능 스크립트 소스

--- src/main/resources/static/js/board/boarddetail.js
+++ src/main/resources/static/js/board/boarddetail.js
@@ -20,3 +20,57 @@ function pwdCheck(){
     }
     return true;
 }
+
+//게시판 좋아요 카운팅
+function likeCount(){
+    let str = '';
+    let id = $('#board_id').val();
+    $.ajax({
+        url:"/api/like/"+id,
+        type:"get",
+        dataType:"json",
+        contentType:"application/json; charset=utf-8"
+    }).done(function (resp) {
+        if(resp.data[0]!=null){
+
+            if(resp.data[1] != false){
+                str += '<i class="fa-sharp fa-solid fa-thumbs-up">' + resp.data[0] + '</i>';
+            }
+            if(resp.data[1] != true){
+                likeCancel();
+                str += '<i class="fa-regular fa-thumbs-up">'+resp.data[0]+'</i>';
+            }
+        }
+        $('#favoriteCount').html(str);
+    });
+}
+//게시판 좋아요 추가하기.
+function boardLike(){
+    console.log("좋아요");
+    let id = $('#board_id').val();
+    $.ajax({
+        url:"/api/like/plus/"+id,
+        type: 'post',
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(id)
+    }).done(function(resp){
+        alert(resp.data);
+        $('#favoriteCount').empty();
+        likeCount();
+    });
+}
+//좋아요 감소
+function likeCancel(){
+    let id = $('#board_id').val();
+    $.ajax({
+        url:'/api/like/minus/'+id,
+        type:'delete',
+        dataType:'json',
+        contentType:'application/json; charset=utf-8'
+    }).done(function(resp){
+        console.log(resp.data);
+        $('#favoriteCount').empty();
+        likeCount();
+    });
+}
