8b5253fe0717ba97d75eff94d287b03b0ed65726
well0924 <well414965@gmail.com>
2023-06-18 17:17:14 +0900

    feat:회원 가입 주소 위경도 추출

---
+++ src/main/java/com/example/coffies_vol_02/favoritePlace/domain/dto/FavoritePlaceResponseDto.java
@@ -0,0 +1,38 @@
+package com.example.coffies_vol_02.favoritePlace.domain.dto;
+
+import com.example.coffies_vol_02.favoritePlace.domain.FavoritePlace;
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.*;
+
+@Getter
+@ToString
+@NoArgsConstructor
+public class FavoritePlaceResponseDto {
+    private Integer id;
+    private Integer placeId;
+    private Integer memberId;
+    private String placeName;
+    private Double reviewRate;
+    private String placeStart;
+    private String placeClose;
+    private String placeAddr1;
+    private String placeAddr2;
+    private String isTitle;
+    private String thumbFileImagePath;
+
+    @Builder
+    @QueryProjection
+    public FavoritePlaceResponseDto (FavoritePlace favoritePlace){
+        this.id = favoritePlace.getId();
+        this.memberId = favoritePlace.getMember().getId();
+        this.placeId = favoritePlace.getPlace().getId();
+        this.placeName = favoritePlace.getPlace().getPlaceName();
+        this.reviewRate = favoritePlace.getPlace().getReviewRate();
+        this.placeStart = favoritePlace.getPlace().getPlaceStart();
+        this.placeClose = favoritePlace.getPlace().getPlaceClose();
+        this.placeAddr1 = favoritePlace.getPlace().getPlaceAddr1();
+        this.placeAddr2 = favoritePlace.getPlace().getPlaceAddr2();
+        this.isTitle = favoritePlace.getPlace().getPlaceImageList().get(0).getIsTitle();
+        this.thumbFileImagePath = favoritePlace.getPlace().getPlaceImageList().get(0).getThumbFileImagePath();
+    }
+}
