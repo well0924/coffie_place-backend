430153b9ed2ade097ed555d590cfb082a1024f67
well0924 <well414965@gmail.com>
2023-06-21 02:08:21 +0900

    feat : 가게 검색어 저장 유효기간 추가

--- src/main/java/com/example/coffies_vol_02/config/redis/RedisService.java
+++ src/main/java/com/example/coffies_vol_02/config/redis/RedisService.java
@@ -5,8 +5,11 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.data.redis.core.*;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
 import java.time.Duration;
+import java.time.ZonedDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 @Log4j2
@@ -16,7 +19,6 @@ public class RedisService {
 
     private final StringRedisTemplate stringRedisTemplate;
     private final RedisTemplate<Object, Object> redisTemplate;
-    private final RedisTemplate<String, String> hashRedisTemplate;
 
     public void setValues(String key, String value, Duration duration) {
         ValueOperations<String, String> values = stringRedisTemplate.opsForValue();
@@ -25,23 +27,25 @@ public class RedisService {
 
     // key word 를 위한 setValues
     public void setValues(String key, String keyword) {
-        ListOperations listOperations = redisTemplate.opsForList();
-        for (Object pastKeyword : listOperations.range(key, 0, listOperations.size(key))) {
+        ListOperations<Object, Object> listOperations = redisTemplate.opsForList();
+        for (Object pastKeyword : Objects.requireNonNull(listOperations.range(key, 0, listOperations.size(key)))) {
             if (String.valueOf(pastKeyword).equals(keyword)) return;
         }
         if (listOperations.size(key) < 5) {
             listOperations.rightPush(key, keyword);
+            //검색어 저장시 하루가 지나가면 자동으로 검색어를 삭제
+            redisTemplate.expireAt(key, Date.from(ZonedDateTime.now().plusDays(1).toInstant()));
         } else if (listOperations.size(key) == 5) {
             listOperations.leftPop(key);
             listOperations.rightPush(key, keyword);
         }
     }
 
+    //redis 검색어 저장하기.
     public List<String> getSearchList(String key) {
         ListOperations listOperations = redisTemplate.opsForList();
         return listOperations.range(key, 0, listOperations.size(key));
     }
-
 
     //redis에서 값 제거
     public void deleteValues(String key) {
