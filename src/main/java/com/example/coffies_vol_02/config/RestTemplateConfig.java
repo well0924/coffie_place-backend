4619c4aa7d8652437c6812cebaf59b801b6893a2
well0924 <well414965@gmail.com>
2023-07-27 23:20:49 +0900

    feat : Spring retry 설정

--- src/main/java/com/example/coffies_vol_02/config/RestTemplateConfig.java
+++ src/main/java/com/example/coffies_vol_02/config/RestTemplateConfig.java
@@ -2,13 +2,28 @@ package com.example.coffies_vol_02.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.web.client.RestTemplate;
 
+@EnableRetry
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate(){
+    public RestTemplate retryTemplate(){
+
+        FixedBackOffPolicy backOffPolicy = new FixedBackOffPolicy();
+        backOffPolicy.setBackOffPeriod(1000);
+
+        SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
+        retryPolicy.setMaxAttempts(2);
+        RetryTemplate retryTemplate = new RetryTemplate();
+        retryTemplate.setBackOffPolicy(backOffPolicy);
+        retryTemplate.setRetryPolicy(retryPolicy);
+
         return new RestTemplate();
     }
 }
