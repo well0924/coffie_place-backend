3aebd768f53427fd2bad32dfc93915cd93dbfacf
well0924 <well414965@gmail.com>
2023-08-06 03:44:50 +0900

    docs : 첨부파일 주석 추가

--- src/main/java/com/example/coffies_vol_02/attach/repository/AttachRepository.java
+++ src/main/java/com/example/coffies_vol_02/attach/repository/AttachRepository.java
@@ -15,18 +15,22 @@ public interface AttachRepository extends JpaRepository<Attach,Integer> {
 
     /**
      * 자유게시판 첨부파일 목록
+     * 자유게시판을 조회했을 때 첨부파일 목록을 전체 조회
      **/
     @Query("select a from Attach a where a.board.id = :id")
     List<Attach>findAttachBoard(@Param("id") Integer boardId)throws Exception;
     
     /**
      * 공지게시판 첨부파일 목록
+     * 공지게시판을 조회했을 때 첨부파일 목록을 전체 조회
      **/
     @Query("select a from Attach a where a.noticeBoard.id = :id")
     List<Attach>findAttachNoticeBoard(@Param("id") Integer noticeId)throws Exception;
     
     /**
-     * 자유게시판 첨부파일 조회
+     * 첨부파일 조회
+     * 게시판 상세조회 페이지에서 파일을 다운로드 할 때 개별 첨부파일을 조회
+     * @param originFileName 첨부파일의 원본파일명
      **/
     Optional<Attach>findAttachByOriginFileName(String originFileName);
 }
