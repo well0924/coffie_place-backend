c976119b57f181f38515a5fa9d36ea8466e3148c
well0924 <well414965@gmail.com>
2023-08-29 15:29:59 +0900

    feat : email 인증 비동기 기능

--- src/main/java/com/example/coffies_vol_02/Application.java
+++ src/main/java/com/example/coffies_vol_02/Application.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAsync
 @EnableJpaAuditing
 @EnableScheduling //스케줄러 활성화
 @EnableCaching //캐시 활성화
